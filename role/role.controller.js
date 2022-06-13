e1152092db9fa2d7b661575577368a354536b492
johnreycanillo <johnreycanillo@gmail.com>
2022-06-13 19:12:25 +0800

    update comment unauthorized

--- role/role.controller.js
+++ role/role.controller.js
@@ -30,10 +30,11 @@ function getAll(res, next) {
 }
 
 function getById(req, res, next) {
+    
     // users can get their own account and admins can get any account
-    if (req.params.id !== req.user.id && req.user.role !== Role.Admin) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
+    //if (req.params.id !== req.user.id && req.user.role !== Role.Admin) {
+        //return res.status(401).json({ message: 'Unauthorized' });
+    //}
 
     roleService.getById(req.params.id)
         .then(role => role ? res.json(role) : res.sendStatus(404))
@@ -64,7 +65,7 @@ function updateSchema(req, next) {
 
     // only admins can update role
     if (req.user.role === Role.Admin) {
-        schemaRules.role = Joi.string().valid(Role.Admin, Role.User).empty('');
+    schemaRules.role = Joi.string().valid(Role.Admin, Role.User).empty('');
     }
 
     const schema = Joi.object(schemaRules).with('password', 'confirmPassword');
@@ -73,9 +74,9 @@ function updateSchema(req, next) {
 
 function update(req, res, next) {
     // users can update their own account and admins can update any account
-    if (req.params.id !== req.user.id && req.user.role !== Role.Admin) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
+    //if (req.params.id !== req.user.id && req.user.role !== Role.Admin) {
+        //return res.status(401).json({ message: 'Unauthorized' });
+    //}
 
     roleService.update(req.params.id, req.body)
         .then(role => res.json(role))
@@ -84,9 +85,9 @@ function update(req, res, next) {
 
 function _delete(req, res, next) {
     // users can delete their own account and admins can delete any account
-    if (req.params.id !== req.user.id && req.user.role !== Role.Admin) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
+    //if (req.params.id !== req.user.id && req.user.role !== Role.Admin) {
+        //return res.status(401).json({ message: 'Unauthorized' });
+    //}
 
     roleService.delete(req.params.id)
         .then(() => res.json({ message: 'Role deleted successfully' }))
